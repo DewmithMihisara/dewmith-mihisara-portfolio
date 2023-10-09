b02b3e02da7472bfcbbabdc58d88c13d31fffd59
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-10-09 20:42:25 +0530

    assignment(js-cal)v2.3.0

--- assignments/js/cal/assets/js/cal_script.js
+++ assignments/js/cal/assets/js/cal_script.js
@@ -1,35 +1,36 @@
 const buttons = document.querySelectorAll('.btn');
 const display = document.getElementById('main_num');
 let currentInput = '0';
-let calValue='0';
+let calValue = '0';
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const buttonValue = button.innerText;
-        if (buttonValue==='C'){
+        if (buttonValue === 'C') {
             currentInput = '0';
-            calValue="0";
-        }else if (buttonValue==='='){
+            calValue = "0";
+        } else if (buttonValue === '=') {
             try {
-                let value=calValue+currentInput;
-                currentInput = eval(value);
+                calValue += currentInput;
+                currentInput = eval(calValue);
+                calValue = '0';
             } catch (error) {
                 currentInput = 'Error';
             }
-        }else if(buttonValue==='⇦'){
+        } else if (buttonValue === '⇦') {
             currentInput = currentInput.slice(0, -1);
             if (currentInput === '') {
                 currentInput = '0';
             }
-        }else if(buttonValue==='+'|| buttonValue==='-' || buttonValue==='/' || buttonValue==='*'){
+        } else if (buttonValue === '+' || buttonValue === '-' || buttonValue === '/' || buttonValue === '*') {
             if (currentInput === '0' || currentInput === 'Error') {
                 currentInput = buttonValue;
             } else {
                 currentInput += buttonValue;
-                calValue+=currentInput;
-                currentInput='0';
+                calValue += currentInput;
+                currentInput = '0';
             }
-        }else{
+        } else {
             if (currentInput === '0' || currentInput === 'Error') {
                 currentInput = buttonValue;
             } else {
